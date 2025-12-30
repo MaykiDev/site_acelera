9ded5260722aed0addbd0b6bcc8411b936290d09
Mayki Yudi Hashimoto <maykihashimoto.dev@gmail.com>
2025-12-30 11:00:50 -0400

    Adicionei programação para relogio

--- script.js
+++ script.js
@@ -20,3 +20,26 @@ function toggleMenu(event) {
 
 btnMobile.addEventListener('click', toggleMenu);
 btnMobile.addEventListener('touchstart', toggleMenu);
+
+// Configuração do Timer
+const dataEvento = new Date("Apr 10, 2026 13:00:00").getTime();
+
+const atualizarTimer = setInterval(() => {
+    const agora = new Date().getTime();
+    const distancia = dataEvento - agora;
+
+    const meses = Math.floor(distancia / (1000 * 60 * 60 * 24 * 30));
+    const dias = Math.floor((distancia % (1000 * 60 * 60 * 24 * 30)) / (1000 * 60 * 60 * 24));
+    const horas = Math.floor((distancia % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutos = Math.floor((distancia % (1000 * 60 * 60)) / (1000 * 60));
+
+    document.getElementById("meses").innerText = meses < 10 ? "0" + meses : meses;
+    document.getElementById("dias").innerText = dias < 10 ? "0" + dias : dias;
+    document.getElementById("horas").innerText = horas < 10 ? "0" + horas : horas;
+    document.getElementById("minutos").innerText = minutos < 10 ? "0" + minutos : minutos;
+
+    if (distancia < 0) {
+        clearInterval(atualizarTimer);
+        document.getElementById("timer").innerHTML = "EVENTO INICIADO";
+    }
+}, 1000);
